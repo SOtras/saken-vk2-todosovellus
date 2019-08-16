6c7d0945cb763fd2af11ac6cc7f5ed4195bac6d7
Sakari Otras <sakari@otras.net>
2019-08-16 23:40:54 +0300

    Osa 3

--- application/__init__.py
+++ application/__init__.py
@@ -4,10 +4,12 @@ app = Flask(__name__)
 
 # Tuodaan SQLAlchemy käyttöön
 from flask_sqlalchemy import SQLAlchemy
+
 # Käytetään tasks.db-nimistä SQLite-tietokantaa. Kolme vinoviivaa
 # kertoo, tiedosto sijaitsee tämän sovelluksen tiedostojen kanssa
 # samassa paikassa
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"
+
 # Pyydetään SQLAlchemyä tulostamaan kaikki SQL-kyselyt
 app.config["SQLALCHEMY_ECHO"] = True
 
@@ -20,5 +22,27 @@ from application import views
 from application.tasks import models
 from application.tasks import views
 
-# Luodaan lopulta tarvittavat tietokantataulut
+from application.auth import views
+
+# Ladataan User-luokka auth-kansiosta
+from application.auth import models
+
+# Kirjautuminen
+
+from application.auth.models import User
+from os import urandom
+app.config["SECRET_KEY"] = urandom(32)
+
+from flask_login import LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = "auth_login"
+login_manager.login_message = "Please login to use this functionality"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+# Luodaan taulut tietokantaa tarvittaessa
 db.create_all()
