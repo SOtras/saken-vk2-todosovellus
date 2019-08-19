ed90a8326c0776c0590de2387b24858fff3b24d0
Sakari Otras <sakari@otras.net>
2019-08-19 19:12:39 +0300

    Osa 3.3

--- application/__init__.py
+++ application/__init__.py
@@ -1,34 +1,28 @@
-# Tuodaan Flask käyttöön
 from flask import Flask
 app = Flask(__name__)
 
-# Tuodaan SQLAlchemy käyttöön
 from flask_sqlalchemy import SQLAlchemy
 
-# Käytetään tasks.db-nimistä SQLite-tietokantaa. Kolme vinoviivaa
-# kertoo, tiedosto sijaitsee tämän sovelluksen tiedostojen kanssa
-# samassa paikassa
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"
+import os
 
-# Pyydetään SQLAlchemyä tulostamaan kaikki SQL-kyselyt
-app.config["SQLALCHEMY_ECHO"] = True
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"    
+    app.config["SQLALCHEMY_ECHO"] = True
 
-# Luodaan db-olio, jota käytetään tietokannan käsittelyyn
 db = SQLAlchemy(app)
 
-# Luetaan kansiosta application tiedoston views sisältö
 from application import views
 
 from application.tasks import models
 from application.tasks import views
 
+from application.auth import models
 from application.auth import views
 
-# Ladataan User-luokka auth-kansiosta
-from application.auth import models
 
-# Kirjautuminen
-
+# login
 from application.auth.models import User
 from os import urandom
 app.config["SECRET_KEY"] = urandom(32)
@@ -38,11 +32,14 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 login_manager.login_view = "auth_login"
-login_manager.login_message = "Please login to use this functionality"
+login_manager.login_message = "Please login to use this functionality."
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
 
-# Luodaan taulut tietokantaa tarvittaessa
-db.create_all()
+
+try: 
+    db.create_all()
+except:
+    pass
